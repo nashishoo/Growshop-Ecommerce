2a141da3c9405450d5a98bbc88b2f84a74c26cdd
Ignacio <ignacio@local>
2026-02-11 01:21:11 -0300

    chore: remove root duplicates and temp files

--- extract_images.py
+++
@@ -1,8 +0,0 @@
-import re
-
-with open('gallery.html', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-matches = re.findall(r'data-pswp-src="([^"]+)"', content)
-for match in matches:
-    print(match)
